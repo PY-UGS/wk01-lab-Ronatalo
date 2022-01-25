b99898943422cd9d1b9a983edacb6528ba405594
Ronatalo <94171025+Ronatalo@users.noreply.github.com>
2022-01-25 21:25:53 +0800

    Add files via upload

---
+++ Question2b.java
@@ -0,0 +1,29 @@
+public class Question2b {
+    public static void main(String[] args){
+        String modulecode = "CSC1009";   
+        switch(modulecode)   
+        {   
+            case "CSC1006":   
+            System.out.println("Mathematics 2"); 
+            break;  
+
+            case "CSC1007":   
+            System.out.println("Operating Systems"); 
+            break;  
+
+            case "CSC1008":   
+            System.out.println("Data Structures and Algorithmns"); 
+            break;  
+
+            case "CSC1009":   
+            System.out.println("Object-Oriented programming"); 
+            break;   
+
+            case "CSC1010":   
+            System.out.println("Computer Networks"); 
+            break;
+                       
+        }   
+    }
+    
+}
